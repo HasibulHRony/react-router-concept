421082211720ed0e8562ea601f7914fcfc6e8c96
HasibulHRony <hasibulhrony7@gmail.com>
2025-10-01 19:05:35 +0600

    going one route from another is added

--- src/Components/PostDetails/PostDetails.jsx
+++ src/Components/PostDetails/PostDetails.jsx
@@ -1,13 +1,19 @@
 import React from 'react';
-import { useLoaderData } from 'react-router';
+import { useLoaderData, useNavigate } from 'react-router';
 
 const PostDetails = () => {
     const postDetailsData = useLoaderData()
     const { title, body} = postDetailsData
+    let navigate = useNavigate()
+
+    const navigateHandler = () =>{
+        navigate('/posts')
+    }
     return (
         <div style={{maxWidth: "480px", border: "2px solid yellow", padding: "10px", borderRadius: "10px", margin: "10px"}}>
             <h2>{title}</h2>
             <p>{body}</p>
+            <button onClick={navigateHandler}>Close</button>
         </div>
     );
 };
