8bf45d685ed51c35af4f425668a1014855bfd140
HasibulHRony <hasibulhrony7@gmail.com>
2025-10-01 19:18:39 +0600

    Loading messege added

--- src/Components/Root/Root.jsx
+++ src/Components/Root/Root.jsx
@@ -1,16 +1,21 @@
 import React from 'react';
 import Header from '../Header/Header';
-import { Outlet } from 'react-router';
+import { Outlet, useNavigation } from 'react-router';
 import Footer from '../Footer/Footer';
 import AsideNav from '../AsideNav/AsideNav';
 import './Root.css'
 
 const Root = () => {
+    const navigation = useNavigation()
+    const isNavigation = Boolean(navigation.location)
     return (
         <div>
             <Header></Header>
             <div className='root-aside'>
                 <AsideNav></AsideNav>
+                {
+                    isNavigation && <span>Loading....</span>
+                }
                 <Outlet></Outlet>
             </div>
             <Footer></Footer>
